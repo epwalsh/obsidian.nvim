0f132e5c3c5794283525c4a45d6faae0bb6e6d10
Pete <epwalsh10@gmail.com>
2023-01-09 10:18:38 -0800

    Move scandir functionality to util func (#79)

--- test/obsidian/util_spec.lua
+++ test/obsidian/util_spec.lua
@@ -1,4 +1,5 @@
 local util = require "obsidian.util"
+local Path = require "plenary.path"
 
 describe("obsidian.util", function()
   it("should correctly URL-encode a path", function()
@@ -34,5 +35,15 @@ describe("obsidian.util", function()
       assert.equals(indices[i][1], expected_indices[i][1])
       assert.equals(indices[i][2], expected_indices[i][2])
     end
+  end)
+  it("should recursively find notes in a directory given a file name", function()
+    local matches = util.find_note(".", "foo.md")
+    assert.equals(#matches, 1)
+    assert.equals(tostring(matches[1]), "./test_fixtures/notes/foo.md")
+  end)
+  it("should recursively find notes in a directory given a partial path", function()
+    local matches = util.find_note(".", "notes/foo.md")
+    assert.equals(#matches, 1)
+    assert.equals(tostring(matches[1]), "./test_fixtures/notes/foo.md")
   end)
 end)
