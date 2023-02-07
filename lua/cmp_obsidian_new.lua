50a965f47af5e856c1a5e1a6cdfc13263557956a
afaikiac <afaikiac@tuta.io>
2023-02-07 06:23:42 +0300

    fix cmp_obsidian_new

--- lua/cmp_obsidian_new.lua
+++ lua/cmp_obsidian_new.lua
@@ -56,7 +56,7 @@ source.execute = function(_, item, callback)
   local data = item.data
   local client = obsidian.new(data.opts)
   client:new_note(data.title, data.id, vim.fn.expand "%:p:h")
-  return callback
+  return callback { }
 end
 
 ---Get opts.
