4bc10b8cb59fd78e87db6b2761b9970702176c67
Pete <epwalsh10@gmail.com>
2023-03-24 16:21:29 -0700

    fix bug when nvim-cmp not installed (#111)

--- after/plugin/cmp_obsidian.lua
+++ after/plugin/cmp_obsidian.lua
@@ -1,1 +1,4 @@
-require("cmp").register_source("obsidian", require("cmp_obsidian").new())
+local has_cmp, cmp = pcall(require, "cmp")
+if has_cmp then
+  cmp.register_source("obsidian", require("cmp_obsidian").new())
+end
