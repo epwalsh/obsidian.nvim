ab1f7f2393c165d334b6f39ad6026e5f583ea1a2
Hawtian <twistoy.wang@gmail.com>
2023-08-03 17:14:15 +0800

    fix bug in template check if there's special chars in path

--- lua/obsidian/init.lua
+++ lua/obsidian/init.lua
@@ -117,7 +117,10 @@ obsidian.setup = function(opts)
   --- @type fun(match: string): boolean
   local is_template
   if self.opts.templates ~= nil then
-    local templates_pattern = "^" .. tostring(self.dir / self.opts.templates.subdir / ".*")
+    local templates_pattern = tostring(self.dir / self.opts.templates.subdir)
+    -- escape special characters
+    templates_pattern = templates_pattern:gsub("([%(%)%.%%%+%-%*%?%[%^%$])", "%%%1")
+    templates_pattern = "^" .. templates_pattern .. ".*"
     is_template = function(match)
       return string.find(match, templates_pattern) ~= nil
     end
