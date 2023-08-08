acd5f2304c646089049d405768c2765df0b705f9
Wes Koerber <wes@weskoerber.com>
2023-08-08 19:36:00 -0400

    chore: workspace class

---
+++ lua/obsidian/workspace.lua
@@ -0,0 +1,71 @@
+---@class obsidian.Workspace
+---@field name string
+---@field path string
+---@return obsidian.Workspace
+local workspace = {}
+
+---Create a new workspace
+---
+---@param name string
+---@param path string
+---
+---@return obsidian.Workspace
+workspace.new = function(name, path)
+  local self = setmetatable({}, { __index = workspace })
+
+  self.name = name
+  self.path = vim.fs.normalize(path)
+
+  return self
+end
+
+workspace.new_from_cwd = function()
+  return workspace.new(".", vim.fn.getcwd())
+end
+
+---Determines if cwd is a workspace
+---
+---@param workspaces table
+---@return obsidian.Workspace|nil
+workspace.get_workspace_from_cwd = function(workspaces)
+  local cwd = vim.fn.getcwd()
+  local _, value = next(vim.tbl_filter(function(w)
+    if w.path == cwd then
+      return true
+    end
+    return false
+  end, workspaces))
+
+  return value
+end
+
+---Returns the default workspace
+---
+---@param workspaces table
+---@return obsidian.Workspace|nil
+workspace.get_default_workspace = function(workspaces)
+  local _, value = next(workspaces)
+  return value
+end
+
+---Resolves current workspace from client config
+---
+---@param opts obsidian.config.ClientOpts
+---@return obsidian.Workspace
+workspace.get_from_opts = function(opts)
+  local current_workspace = nil
+
+  if opts.detect_cwd then
+    current_workspace = workspace.get_workspace_from_cwd(opts.workspaces)
+  else
+    current_workspace = workspace.get_default_workspace(opts.workspaces)
+  end
+
+  if not current_workspace then
+    current_workspace = workspace.new_from_cwd()
+  end
+
+  return current_workspace
+end
+
+return workspace
