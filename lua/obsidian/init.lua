1e1ba52e737f2f243fc3ea8b0cb6aca2df9afdd1
Wes Koerber <wes@weskoerber.com>
2023-10-17 14:31:04 -0400

    chore: default workspace name to the name of the directory
    - previously using obfuscatory "test_vault"

--- lua/obsidian/init.lua
+++ lua/obsidian/init.lua
@@ -42,7 +42,8 @@ end
 ---@return obsidian.Client
 obsidian.new_from_dir = function(dir)
   local opts = config.ClientOpts.default()
-  opts.workspaces = vim.tbl_extend("force", { obsidian.workspace.new("test_vault", dir) }, opts.workspaces)
+  opts.workspaces =
+      vim.tbl_extend("force", { obsidian.workspace.new_from_dir(dir) }, opts.workspaces)
   return obsidian.new(opts)
 end
 
