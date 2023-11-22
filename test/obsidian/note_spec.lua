e1ae6030599da414907085f24641cc8083f288f4
epwalsh <petew@allenai.org>
2023-11-22 12:54:35 -0800

    fix test, update doc build

--- test/obsidian/note_spec.lua
+++ test/obsidian/note_spec.lua
@@ -19,8 +19,6 @@ describe("Note", function()
   it("should be able to be initialized from a README", function()
     local note = Note.from_file "README.md"
     assert.equals(note.id, "README")
-    assert.equals(#note.aliases, 1)
-    assert.equals(note.aliases[1], "obsidian.nvim")
     assert.equals(#note.tags, 0)
     assert.equals(note:fname(), "README.md")
     assert.is_false(note:should_save_frontmatter())
