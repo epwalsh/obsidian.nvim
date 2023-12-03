49520ed767e8b0b5fda853699138dae7034ad902
adam <adam@nodomain>
2023-12-03 21:19:29 +0100

    Implement `workspace.register` and `workspace.clear`

--- lua/obsidian/workspace.lua
+++ lua/obsidian/workspace.lua
@@ -3,6 +3,7 @@ local abc = require "obsidian.abc"
 ---@class obsidian.Workspace : obsidian.ABC
 ---@field name string
 ---@field path string
+---@field group number autocommand group id, used by `Workspace.register`
 local Workspace = abc.new_class {
   __tostring = function(self)
     return string.format("Workspace('%s', '%s')", self.name, self.path)
@@ -19,6 +20,7 @@ Workspace.new = function(name, path)
   local self = Workspace.init()
   self.name = name
   self.path = vim.fs.normalize(path)
+  self.group = vim.api.nvim_create_augroup("obsidian_workspace_" .. string.lower(name), { clear = true })
   return self
 end
 
@@ -26,6 +28,10 @@ Workspace.new_from_cwd = function()
   return Workspace.new_from_dir(vim.fn.getcwd())
 end
 
+---Create new workspace
+--
+---@param dir string
+---@return obsidian.Workspace
 Workspace.new_from_dir = function(dir)
   return Workspace.new(vim.fn.fnamemodify(dir, ":t"), dir)
 end
@@ -73,6 +79,25 @@ Workspace.get_from_opts = function(opts)
   end
 
   return current_workspace
+end
+
+--- register `cb` to be called whenever a buffer from this workspace is entered
+--- all callbacks can be cleared with `Workspace:clear()`
+---@param self obsidian.Workspace
+---@param cb fun(cb: fun(event:any))
+Workspace.register = function(self, cb)
+  vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    group = self.group,
+    pattern = tostring(self.path / "**.md"),
+    callback = cb
+  })
+end
+
+--- Clear autocommands
+---
+---@param self obsidian.Workspace
+Workspace.clear = function(self)
+  vim.api.nvim_clear_autocmds({ group = self.group })
 end
 
 return Workspace
