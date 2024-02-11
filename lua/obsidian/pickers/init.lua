06e62fda5834681fbd3569fcf3de909f4987c737
epwalsh <petew@allenai.org>
2024-02-11 11:31:31 -0800

    Allow `picker.name` to be `nil`
    Fixes #389.

--- lua/obsidian/pickers/init.lua
+++ lua/obsidian/pickers/init.lua
@@ -5,10 +5,23 @@ local M = {}
 --- Get the default Picker.
 ---
 ---@param client obsidian.Client
+---@param picker_name obsidian.config.Picker|?
 ---
 ---@return obsidian.Picker|?
-M.get = function(client)
-  local picker_name = string.lower(client.opts.picker.name)
+M.get = function(client, picker_name)
+  picker_name = picker_name and picker_name or client.opts.picker.name
+  if picker_name then
+    picker_name = string.lower(picker_name)
+  else
+    for _, name in ipairs { PickerName.telescope, PickerName.fzf_lua, PickerName.mini } do
+      local ok, res = pcall(M.get, client, name)
+      if ok then
+        return res
+      end
+    end
+    return nil
+  end
+
   if picker_name == string.lower(PickerName.telescope) then
     return require("obsidian.pickers._telescope").new(client)
   elseif picker_name == string.lower(PickerName.mini) then
@@ -17,8 +30,6 @@ M.get = function(client)
     return require("obsidian.pickers._fzf").new(client)
   elseif picker_name then
     error("not implemented for " .. picker_name)
-  else
-    return nil
   end
 end
 
