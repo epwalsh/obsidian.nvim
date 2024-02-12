34128182ba4222ecce5d5f958838e15ec521a82e
epwalsh <petew@allenai.org>
2024-02-12 12:39:42 -0800

    Improve how we collect visual selections
    See #372.

--- lua/obsidian/commands/link_new.lua
+++ lua/obsidian/commands/link_new.lua
@@ -1,37 +1,30 @@
 local log = require "obsidian.log"
+local util = require "obsidian.util"
 
 ---@param client obsidian.Client
 return function(client, data)
-  local _, csrow, cscol, _ = unpack(vim.fn.getpos "'<")
-  local _, cerow, cecol, _ = unpack(vim.fn.getpos "'>")
-
-  if data.line1 ~= csrow or data.line2 ~= cerow then
+  local viz = util.get_visual_selection()
+  if viz.lines == nil or #viz.lines == 0 then
     log.err "ObsidianLink must be called with visual selection"
     return
-  end
-
-  local lines = vim.fn.getline(csrow, cerow)
-  if #lines ~= 1 then
+  elseif #viz.lines ~= 1 then
     log.err "Only in-line visual selections allowed"
     return
   end
 
-  local line = lines[1]
+  local line = assert(viz.lines[1])
 
   local title
   if string.len(data.args) > 0 then
     title = data.args
   else
-    title = string.sub(line, cscol, cecol)
+    title = viz.selection
   end
   local note = client:new_note(title, nil, client.buf_dir)
 
-  line = string.sub(line, 1, cscol - 1)
-    .. "[["
-    .. tostring(note.id)
-    .. "|"
-    .. string.sub(line, cscol, cecol)
-    .. "]]"
-    .. string.sub(line, cecol + 1)
-  vim.api.nvim_buf_set_lines(0, csrow - 1, csrow, false, { line })
+  local new_line = string.sub(line, 1, viz.cscol - 1)
+    .. client:format_link(note, { label = title })
+    .. string.sub(line, viz.cecol + 1)
+
+  vim.api.nvim_buf_set_lines(0, viz.csrow - 1, viz.csrow, false, { new_line })
 end
--- lua/obsidian/util.lua
+++ lua/obsidian/util.lua
@@ -801,4 +801,62 @@ util.buf_get_full_text = function(bufnr)
   return text
 end
 
+--- Get the current visual selection of text and exit visual mode.
+---
+---@return { lines: string[], selection: string, csrow: integer, cscol: integer, cerow: integer, cecol: integer }
+util.get_visual_selection = function()
+  -- Adapted from fzf-lua:
+  -- https://github.com/ibhagwan/fzf-lua/blob/6ee73fdf2a79bbd74ec56d980262e29993b46f2b/lua/fzf-lua/utils.lua#L434-L466
+  -- this will exit visual mode
+  -- use 'gv' to reselect the text
+  local _, csrow, cscol, cerow, cecol
+  local mode = vim.fn.mode()
+  if mode == "v" or mode == "V" or mode == "" then
+    -- if we are in visual mode use the live position
+    _, csrow, cscol, _ = unpack(vim.fn.getpos ".")
+    _, cerow, cecol, _ = unpack(vim.fn.getpos "v")
+    if mode == "V" then
+      -- visual line doesn't provide columns
+      cscol, cecol = 0, 999
+    end
+    -- exit visual mode
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+  else
+    -- otherwise, use the last known visual position
+    _, csrow, cscol, _ = unpack(vim.fn.getpos "'<")
+    _, cerow, cecol, _ = unpack(vim.fn.getpos "'>")
+  end
+
+  -- swap vars if needed
+  if cerow < csrow then
+    csrow, cerow = cerow, csrow
+  end
+  if cecol < cscol then
+    cscol, cecol = cecol, cscol
+  end
+
+  local lines = vim.fn.getline(csrow, cerow)
+  assert(type(lines) == "table")
+
+  ---@type string
+  local selection
+  local n = #lines
+  if n <= 0 then
+    selection = ""
+  elseif n == 1 then
+    selection = string.sub(lines[1], cscol, cecol)
+  else
+    selection = string.sub(lines[1], cscol) .. table.concat(lines, "\n", 2, n - 1) .. string.sub(lines[n], 1, cecol)
+  end
+
+  return {
+    lines = lines,
+    selection = selection,
+    csrow = csrow,
+    cscol = cscol,
+    cerow = cerow,
+    cecol = cecol,
+  }
+end
+
 return util
