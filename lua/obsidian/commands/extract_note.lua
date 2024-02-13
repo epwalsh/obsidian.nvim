31f8ca3615f14fe0a7d5bc287fb10a2f28f5343d
Stefan Otte <stefan.otte@gmail.com>
2024-02-13 23:06:33 +0100

    CR: only replace the selected text, not the entire line
    Co-authored-by: Pete <epwalsh10@gmail.com>

--- lua/obsidian/commands/extract_note.lua
+++ lua/obsidian/commands/extract_note.lua
@@ -28,7 +28,7 @@ return function(client, data)
 
   -- replace selection with link to new note
   local link = client:format_link(note)
-  vim.api.nvim_buf_set_lines(0, viz.csrow - 1, viz.cerow + 1, false, { link })
+  vim.api.nvim_buf_set_text(0, viz.csrow - 1, viz.cscol - 1, viz.cerow - 1, viz.cecol, { link })
 
   -- add the selected text to the end of the new note
   local open_in = util.get_open_strategy(client.opts.open_notes_in)
