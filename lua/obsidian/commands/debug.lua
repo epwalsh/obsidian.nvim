d8d160d72c92b42c638f994d87c85317f3ffd5d8
epwalsh <petew@allenai.org>
2024-02-22 15:26:52 -0800

    Clean up debug cmd more

--- lua/obsidian/commands/debug.lua
+++ lua/obsidian/commands/debug.lua
@@ -8,16 +8,10 @@ return function(client)
   log.lazy_info("  Working directory: '%s'", vim.fn.getcwd())
 
   log.lazy_info "Workspaces:"
-  local cur_workspace = Workspace.get_workspace_for_dir(client.buf_dir, client.opts.workspaces)
-  if cur_workspace ~= nil then
-    log.lazy_info("  Active workspace: %s", cur_workspace)
-  else
-    log.lazy_info "  No active workspaces"
-  end
-
+  log.lazy_info("  Active workspace: %s", client.current_workspace)
   for _, workspace_spec in ipairs(client.opts.workspaces) do
     local workspace = Workspace.new_from_spec(workspace_spec)
-    if workspace ~= cur_workspace then
+    if workspace ~= client.current_workspace then
       log.lazy_info("  Inactive workspace: %s", workspace)
     end
   end
