3e2128ffa84eefc44bb7489d1452ca6380837109
CaeChao <winston@An0nym0us1q84>
2024-03-27 14:25:16 -0400

    Fix: change toggle_checkbox default opts & make opts' sorting function more robust

--- lua/obsidian/commands/checkbox.lua
+++ lua/obsidian/commands/checkbox.lua
@@ -3,12 +3,8 @@ local toggle_checkbox = require("obsidian.util").toggle_checkbox
 ---@param client obsidian.Client
 return function(client)
   local checkboxes = vim.tbl_keys(client.opts.ui.checkboxes)
-  for k, v in pairs(client.opts.ui.checkboxes) do
-    local order = v.order
-    if order and type(order) == "number" then
-      -- sort the checkboxes based on order
-      checkboxes[order] = k
-    end
-  end
+  table.sort(checkboxes, function(a, b)
+    return client.opts.ui.checkboxes[a].order < client.opts.ui.checkboxes[b].order
+  end)
   toggle_checkbox(checkboxes)
 end
