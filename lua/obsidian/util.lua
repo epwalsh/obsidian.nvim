epwalsh/obsidian.nvim

commit 6ffd1964500d24c1adde3c88705b146d9c415207

fix: os_uname check for wsl (#543)

lua/obsidian/util.lua
@@ -437,9 +437,9 @@ util.get_os = function()
   if vim.fn.has "win32" == 1 then
     this_os = util.OSType.Windows
   else
-    local sysname = vim.loop.os_uname().sysname
-    local release = vim.loop.os_uname().release
-    if sysname == "Linux" and string.find(release, "microsoft") then
+    local sysname = vim.loop.os_uname().sysname:lower()
+    local release = vim.loop.os_uname().release:lower()
+    if sysname == "linux" and string.find(release, "microsoft") then
       this_os = util.OSType.Wsl
     else
       this_os = sysname

commit f9e3309fbe1ce4eca4d329c3b11017496d5e1bea

Fix bug with os detection
Introduced by 6ffd1964500d24c1adde3c88705b146d9c415207.
See
https://github.com/epwalsh/obsidian.nvim/commit/6ffd1964500d24c1adde3c88705b146d9c415207#commitcomment-141147411.

lua/obsidian/util.lua
@@ -437,9 +437,9 @@ util.get_os = function()
   if vim.fn.has "win32" == 1 then
     this_os = util.OSType.Windows
   else
-    local sysname = vim.loop.os_uname().sysname:lower()
+    local sysname = vim.loop.os_uname().sysname
     local release = vim.loop.os_uname().release:lower()
-    if sysname == "linux" and string.find(release, "microsoft") then
+    if sysname:lower() == "linux" and string.find(release, "microsoft") then
       this_os = util.OSType.Wsl
     else
       this_os = sysname