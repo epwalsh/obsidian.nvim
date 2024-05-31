1208a02cbfdec005fa85fc1d1fe32f3df4570f4f
epwalsh <petew@allenai.org>
2024-05-31 08:39:11 -0700

    Fix `compat` function to work in async

--- lua/obsidian/compat.lua
+++ lua/obsidian/compat.lua
@@ -1,7 +1,12 @@
 local compat = {}
 
+local has_nvim_0_11 = false
+if vim.fn.has "nvim-0.11" == 1 then
+  has_nvim_0_11 = true
+end
+
 compat.is_list = function(t)
-  if vim.fn.has "nvim-0.11" == 1 then
+  if has_nvim_0_11 then
     return vim.islist(t)
   else
     return vim.tbl_islist(t)
@@ -9,7 +14,8 @@ compat.is_list = function(t)
 end
 
 compat.flatten = function(t)
-  if vim.fn.has "nvim-0.11" == 1 then
+  if has_nvim_0_11 then
+    ---@diagnostic disable-next-line: undefined-field
     return vim.iter(t):flatten():totable()
   else
     return vim.tbl_flatten(t)
