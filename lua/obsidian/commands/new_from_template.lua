216f4f8b1aafce81b0ea446cb0cf4c999a392c96
Maxime DAFFIS <m.daffis@criteo.com>
2024-06-11 18:01:51 +0200

    [feature] Add ObsidianNewFromTemplate feature
    The goal is to be able to create a new note while using an existing
    template

---
+++ lua/obsidian/commands/new_from_template.lua
@@ -0,0 +1,45 @@
+local util = require "obsidian.util"
+local log = require "obsidian.log"
+
+---@param client obsidian.Client
+return function(client, data)
+  ---@type obsidian.Note
+  if not client:templates_dir() then
+    log.err "Templates folder is not defined or does not exist"
+    return
+  end
+
+  local note
+  if data.args:len() > 0 then
+    note = client:create_note { title = data.args, no_write = true }
+  else
+    local title = util.input("Enter title or path (optional): ", { completion = "file" })
+    if not title then
+      log.warn "Aborted"
+      return
+    elseif title == "" then
+      title = nil
+    end
+    note = client:create_note { title = title, no_write = true }
+  end
+
+  -- Open the note in a new buffer.
+  client:open_note(note, { sync = true })
+
+  local function insert_template(name)
+    client:write_note_to_buffer(note, { template = name })
+  end
+
+  local picker = client:picker()
+  if not picker then
+    log.err "No picker configured"
+    return
+  end
+
+  picker:find_templates {
+    callback = function(path)
+      insert_template(path)
+    end,
+  }
+  client:write_note_to_buffer(note)
+end
