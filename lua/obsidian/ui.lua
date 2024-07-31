6220e17c47a71c00185af989241a84d6e3b418a9
Aquiles Gomez <aquilesg24@gmail.com>
2024-07-30 21:01:04 -0500

    Remove 'stack' implementation

--- lua/obsidian/ui.lua
+++ lua/obsidian/ui.lua
@@ -3,7 +3,6 @@ local util = require "obsidian.util"
 local log = require "obsidian.log"
 local search = require "obsidian.search"
 local DefaultTbl = require("obsidian.collections").DefaultTbl
-local collections = require "obsidian.collections"
 local iter = require("obsidian.itertools").iter
 
 local M = {}
@@ -636,12 +635,12 @@ local function get_callout_extmarks(marks, line, lnum, ui_opts, callout_hl_group
         end
       end
     end
-  elseif string.find(line, ">") and not collections.is_empty(callout_hl_group_stack) then
+  elseif string.find(line, ">") and not #callout_hl_group_stack == 0 then
     log.debug "Callout stack available, generating marks for callout body"
     -- If we have a current stack, then we're in a callout group and should treat the lone
     -- > character as part of a callout block
     generate_callout_extmarks_body(marks, line, lnum, callout_hl_group_stack)
-  elseif not string.match(line, "%s*>(.+)") and not collections.is_empty(callout_hl_group_stack) then
+  elseif not string.match(line, "%s*>(.+)") and not #callout_hl_group_stack == 0 then
     log.debug "Clearing callout stack"
     -- If we have a current stack, but the we don't match the > block, then we should remove all of the items from the stack
     -- as this inidcates we've exited the existing callout block
@@ -703,7 +702,7 @@ local function update_extmarks(bufnr, ns_id, ui_opts)
 
   -- Iterate over lines (skipping code blocks) and update marks.
   local inside_code_block = false
-  local callout_block_highlights = collections.new_stack()
+  local callout_block_highlights = {}
   local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)
   for i, line in ipairs(lines) do
     local lnum = i - 1
@@ -725,17 +724,7 @@ local function update_extmarks(bufnr, ns_id, ui_opts)
     elseif not inside_code_block then
       -- Mark that we have a highlight group now
       if string.match(line, search.Patterns.Callout) then
-        local count = 0
-        for c = 1, #line do
-          local char = line:sub(c, c)
-          if char == ">" then
-            count = count + 1
-          elseif char == "[" then
-            if line:sub(c, c + 2) == "[!]" then
-              break
-            end
-          end
-        end
+        local count = util.string_count(line, "> ")
         local highlight = get_callout_hl_group(line, ui_opts)
         callout_block_highlights[count] = highlight
         log.debug("Inserting callout highlight: " .. highlight .. " into idx: " .. count)
